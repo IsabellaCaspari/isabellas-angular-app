f0803f5acb108d60ce8310979ceb846fda919d29
isabellaCaspari <ica@adorsys.de>
2020-03-16 16:45:13 +0100

    add first route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StartPageComponent } from './start-page/start-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'start', component: StartPageComponent},
+  { path: '', redirectTo: 'start', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
