100b7df0ee68fa0f8d0877c8f5221ba11c4ad99a
isabellaCaspari <ica@adorsys.de>
2020-03-18 14:24:14 +0100

    add tech-skill page

---
+++ src/app/tech-skill-page/tech-skill-page.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { TechSkill } from '../models/techSkill';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-tech-skill-page',
+  templateUrl: './tech-skill-page.component.html',
+  styleUrls: ['./tech-skill-page.component.less']
+})
+export class TechSkillPageComponent implements OnInit {
+  techList: TechSkill[];
+  rate = 7;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.initList();
+  }
+
+  openLink(link: string) {
+    open(link);
+  }
+
+  initList() {
+    this.techList = [
+     {name: 'android', imageLink: '/assets/img/logos/android_logo.png', skillLevel: 90, star: 5, link: "https://developer.android.com"},
+     {name: 'kotlin', imageLink: '/assets/img/logos/kotlin_logo.png', skillLevel: 80, star: 4, link: "https://kotlinlang.org"},
+     {name: 'java', imageLink: '/assets/img/logos/java_logo.png', skillLevel: 80, star: 4,  link: "https://java.com"},
+     {name: 'html', imageLink: '/assets/img/logos/html_css_logo.png', skillLevel: 80, star: 4, link: "https://html.spec.whatwg.org/multipage/"},
+     {name: 'css', imageLink: '/assets/img/logos/css_logo.png', skillLevel: 80, star: 4,  link: "https://www.w3schools.com/css/"},
+     {name: 'angular', imageLink: '/assets/img/logos/angular_logo.png', skillLevel: 60, star: 3,  link: "https://angular.io"},
+     {name: 'adobe', imageLink: '/assets/img/logos/adobe_cloud_logo.png', skillLevel: 60, star: 3,  link: "https://www.adobe.com"}
+    ];
+  }
+
+}
